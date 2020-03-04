31e242409a94a3d88fc52ff3425092542d665672
Bas van Rossem <bas.vanrossem@student.hu.nl>
2020-03-04 20:53:09 +0100

    Fianally a working NN, thanks to Jens and Jasper. Need to refactor

--- Programming_NNs/main.py
+++ Programming_NNs/main.py
@@ -9,132 +9,126 @@ def derivative(x):
     return x * (1 - x)
 
 class Neuron:
-    def __init__(self):
-        self.connections_forward = dict()
-        self.is_input_layer = False
-        self.is_bias = False
-        self.data = 0
-        self.error = 0
+    def __init__(self, inputs, weights, bias):
+        self.delta = 0
+        self.inputs = inputs
+        self.weights = weights
+        self.inproduct = None # Before sigmoid
+        self.bias = bias
+        self.new_weights = None
+        self.new_bias = None
 
-    # Makes a two way connection between the neurons
-    def connect(self, neuron, weight):
-        self.connections_forward[neuron] = weight
+    def activation(self):
+        self.inproduct = np.dot(self.inputs, self.weights) + self.bias
+        return sigmoid(self.inproduct)
 
-    def generate_output(self):
-        for neuron, weight in self.connections_forward.items():
-            neuron.data += self.data * weight
+    def refresh_weights(self):
+        self.weights = self.new_weights
+        self.new_weights = None
+
+    def refresh_bias(self):
+        self.bias = self.new_bias
+        self.new_bias = None
+
     
 class NeuralNetwork:
-    def __init__(self, learning_rate = 1):
-        self.layers = list()
-        
-        print("Creating neural network")
-        self.total_error = 0
+    def __init__(self, layer_layout, learning_rate = 1):
         self.learning_rate = learning_rate
+        self.input_size = 4
+        self.layers = list()
 
-        # Create 4 input Neurons
-        self.layers.append(list())
-        for i in range(4):
-            input_neuron = Neuron()
-            input_neuron.is_input_layer = True
-            self.layers[0].append(input_neuron)
+        for i, neuron_count in enumerate(layer_layout):
+            layer = []
+            for _j in range(neuron_count):
+                weight_size = self.input_size
+                if i > 0:
+                    weight_size = len(self.layers[i-1])
+                weights = np.random.rand(1,weight_size)
+                bias = np.random.random()
+                neuron = Neuron(np.array([]), weights[0], bias)
+                layer.append(neuron)
+            self.layers.append(layer)
 
-        # Create two hidden layers with each two neurons
-        for i in range(2):
-            self.layers.append(list())
-            for j in range(2):
-                self.layers[-1].append(Neuron())
+        print(self.layers)
+    
+    def train(self, values, label):
+        if len(values) != self.input_size:
+            raise ValueError("Input values are not the same size as NN inputs")
+        if len(label) != len(self.layers[-1]):
+            raise ValueError("Label values are not the same size as NN outputs")
 
-        # Create an output layer with 3 neurons
-        self.layers.append(list())
-        for i in range(3):
-            self.layers[-1].append(Neuron())
+        self.feed_forward(values)
+        self.back_propagation(label)
+        self.refresh_all()
 
-        # Connect all neurons with the neurons in the next layer
-        for i in range(len(self.layers) - 1):
-            for neuron in self.layers[i]:
-                for connection in self.layers[i + 1]:
-                    neuron.connect(connection, np.random.random())
-        
-        # Add bias nodes 
-        for i in range(len(self.layers) - 1):
-            bias = Neuron()
-            bias.data = 1
-            bias.is_bias = True
-            for connection in self.layers[i + 1]:
-                bias.connect(connection, np.random.random())
-            self.layers[i].append(bias)   
-
-        print("Creating neural network Done")      
-
-    def train(self, training_input, training_expected, runs):
-        for run in range(runs):
-            self.total_error = 0
-            for index in range(len(training_input)):
-                # Set all data to 0
-                self.reset_data() 
-
-                # Make predictions
-                self.predict(training_input[index])
-
-                # Back propagate
-                self.back_propagate(training_expected[index])
-
-            # if run % 10 == 0:
-            #     print("==========")
-            #     print("Data NN:")
-            #     for layer in self.layers:
-            #         for neuron in layer:
-            #             print(neuron.data)
-            #         print("-------------")
-            print("Total error:", self.total_error)
-
-        print("Network trained")
-
-    def predict(self, inputs):
-        # Add input values to the input layer
-        for neuron_index in range(len(self.layers[0]) - 1):
-            self.layers[0][neuron_index].data = inputs[neuron_index]
-
-        # Feed forward
-        for i in range(len(self.layers)):
-            if i != 0:
-                for neuron in self.layers[i]:
-                    if not neuron.is_bias:
-                        neuron.data = sigmoid(neuron.data)
-            for neuron in self.layers[i]:
-                neuron.generate_output()
-                  
-    def back_propagate(self, expected):
-        # Calculate the error for the neurons in the output layers
-        for neuron_index in range(len(self.layers[-1])):
-            neuron = self.layers[-1][neuron_index]
-            neuron.error = (expected[neuron_index] - neuron.data) * derivative(neuron.data)
-            self.total_error += (expected[neuron_index] - neuron.data) ** 2
-
-        # Calculate the error for the neurons in the hidden layers
-        sub_layers = self.layers[1:-1]
-        sub_layers.reverse()
-        for layer in sub_layers:
+    def feed_forward(self, values):
+        for layer in self.layers:
+            new_values = list()
             for neuron in layer:
-                error = 0
-                for sub_neuron, weight in neuron.connections_forward.items():
-                    error += sub_neuron.error * weight
-                neuron.error = error * derivative(neuron.data)
+                neuron.inputs = values
+                new_values.append(neuron.activation())
+            values = new_values
+    
+    def back_propagation(self, label):
+        all_delta_weights = list()
+        for i, layer in enumerate(reversed(self.layers)):
+            current_weights = list()
+            for j, neuron in enumerate(layer):
+                if i == 0:
+                    self.output_update(neuron, label[j])
+                else:
+                    delta_sum = 0
+                    #Maybe transpose to do sum(list)
+                    for neuron_weight in all_delta_weights[i-1]:
+                        delta_sum += neuron_weight[j]
+                    self.neuron_update(neuron, delta_sum)
 
-        # Apply new weights to the connections
+                neuron_weights = list()
+                for weight in neuron.weights:
+                    neuron_weights.append(neuron.delta * weight)
+                current_weights.append(neuron_weights)
+
+            all_delta_weights.append(current_weights)
+
+    def output_update(self, neuron, label):
+        self.calculate_output_delta(neuron, label)
+        new_weights = []
+        for i, weight in enumerate(neuron.weights):
+            new_weights.append(weight + (self.learning_rate * neuron.delta * neuron.inputs[i]))
+        neuron.new_weights = np.array(new_weights)
+        neuron.new_bias = neuron.bias + (self.learning_rate * neuron.delta)    
+
+    def neuron_update(self, neuron, delta):
+        self.calculate_neuron_delta(neuron, delta)
+        new_weights = []
+        for i, weight in enumerate(neuron.weights):
+            new_weights.append(weight + (self.learning_rate * neuron.delta * neuron.inputs[i]))
+        neuron.new_weights = np.array(new_weights)
+        neuron.new_bias = neuron.bias + (self.learning_rate * neuron.delta)
+
+    def calculate_output_delta(self, neuron, label):
+        neuron.delta = (label - neuron.activation()) * derivative(sigmoid(neuron.inproduct))
+
+    def calculate_neuron_delta(self, neuron, delta):
+        if neuron.inproduct  == None:
+            raise ValueError("No inporduct, something went wrong in the last steps")
+        neuron.delta = (derivative(sigmoid(neuron.inproduct)) * delta)
+
+    def refresh_all(self):
         for layer in self.layers:
             for neuron in layer:
-                for sub_neuron, weight in neuron.connections_forward.items():
-                    if neuron.is_bias:
-                        neuron.connections_forward[sub_neuron] = weight + (self.learning_rate * sub_neuron.error)
-                    else:
-                        neuron.connections_forward[sub_neuron] = weight + (self.learning_rate * sub_neuron.error * neuron.data)
+                neuron.refresh_bias()
+                neuron.refresh_weights()
+    
+    def test(self, input):
+        self.feed_forward(input)
+        results = []
+        for output_neuron in self.layers[-1]:
+            results.append(output_neuron.activation())
 
-    def reset_data(self):
-        for layer in self.layers[1:]:
-            for neuron in layer:
-                neuron.data = 0
+        return results
+
+
 
 np.random.seed(1)
 
@@ -146,13 +140,13 @@ data_in_norm = data_in / data_in.max(axis = 0)
 data_out    = np.genfromtxt('iris.data', delimiter=',', usecols=[4], converters={4: converter})
 error_cutoff = 0.05
 
-NN = NeuralNetwork(3)
-NN.train(data_in_norm, data_out, 2000)
+network = NeuralNetwork([2,2,3], 0.05)
 
-q1 = data_in_norm[0]
-NN.predict(np.array(q1))
+for _i in range(0, 1000):
+    if _i % 10 == 0:
+        print(_i)
+    for i, input in enumerate(data_in_norm):
+        network.train(input, data_out[i])
 
-print("Expected output: ", data_out[0])
-for neuron in NN.layers[-1]:
-    print(neuron.error)
-
+for i, input in enumerate(data_in_norm[:2]):
+    print(network.test(input))
