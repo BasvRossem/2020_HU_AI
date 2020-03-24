b0f024f5cbf705d37c82bce02fe2d59355024c8b
Bas van Rossem <bas.vanrossem@student.hu.nl>
2020-03-24 21:48:10 +0100

    Added printing

--- Algebraic_Representation/main.py
+++ Algebraic_Representation/main.py
@@ -82,6 +82,9 @@ model.fit(x_train, y_train,
           verbose=1,
           validation_data=(x_test, y_test))
 
+#===============================
+# print the scores
+#===============================
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
